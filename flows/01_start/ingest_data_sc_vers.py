cbcabd493f33381e53ef1fbfcb9fdc7630c9277a
stanchiu224 <stanchiu224@gmail.com>
2023-01-31 02:57:58 +0000

    Updated .gitignore to include .gz files

---
+++ flows/01_start/ingest_data_sc_vers.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import argparse
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+from prefect import flow, task
+
+@task(log_prints=True, retries=3)
+def ingest_data(user, password, host, port, db, table_name, url):
+    
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_name = 'yellow_tripdata_2021-01.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
+    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(postgres_url)
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+@flow(name="Ingest Flow")
+def main_flow():
+    user = "root"
+    password = "root"
+    host = "localhost"
+    port = "5432"
+    db = "ny_taxi"
+    table_name = "yellow_taxi_trips"
+    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+    
+    ingest_data(user, password, host, port, db, table_name, csv_url)
+
+if __name__ == '__main__':
+    main_flow()
+
+    
