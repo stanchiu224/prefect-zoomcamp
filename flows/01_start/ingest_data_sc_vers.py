44b9b787e65ce62d8c853c2692a2bb356e462b09
stanchiu224 <stanchiu224@gmail.com>
2023-02-03 01:04:44 +0000

    Added log_subflow and commented code for readability

--- flows/01_start/ingest_data_sc_vers.py
+++ flows/01_start/ingest_data_sc_vers.py
@@ -8,7 +8,9 @@ from sqlalchemy import create_engine
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
+# Extract yellow taxi trip data from the web
 @task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def extract_data(url):
 
@@ -19,12 +21,16 @@ def extract_data(url):
     else:
         csv_name = 'output.csv'
 
+    # download the csv file from the web
     os.system(f"wget {url} -O {csv_name}")
-    
+
+    # break the data into chunks of 100,000 rows, so that it is easier for the db to process 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
+    # assigns each chunk of data into a df
     df = next(df_iter)
 
+    # recognize time data
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
@@ -33,34 +39,30 @@ def extract_data(url):
 @task(log_prints=True)
 def transform_data(df):
     print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    
+    # filter the df so that only rows with passenger_count >0 is included
     df = df[df['passenger_count'] != 0]
     print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
     return df
 
 @task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, df):
-    postgres_url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
-    engine = create_engine(postgres_url)
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+def ingest_data(table_name, df):
+    connection_block = SqlAlchemyConnector.load("postgresql-connector")
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
-
+@flow(name="Subflow", log_prints=True)
+def log_subflow(table_name: str):
+    print(f"Logging Subflow for: {table_name}")
 
 @flow(name="Ingest Flow")
-def main_flow():
-    user = "root"
-    password = "root"
-    host = "localhost"
-    port = "5432"
-    db = "ny_taxi"
-    table_name = "yellow_taxi_trips"
+def main_flow(table_name: str = "yellow_taxi_trips"):
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-    
+    log_subflow(table_name) 
     raw_data = extract_data(csv_url)
     data = transform_data(raw_data)
-    ingest_data(user, password, host, port, db, table_name, data)
+    ingest_data(table_name, data)
 
 if __name__ == '__main__':
-    main_flow()
-
-    
+    main_flow("yellow_taxi_trips")
