929365631fc154b5a3d246335a17c53c47569665
stanchiu224 <stanchiu224@gmail.com>
2023-02-07 03:47:12 +0000

    added personal details, caching, and log prints

--- flows/02_gcp/etl_gcs_to_bq.py
+++ flows/02_gcp/etl_gcs_to_bq.py
@@ -2,37 +2,44 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.tasks import task_input_hash
+from datetime import timedelta
 from prefect_gcp import GcpCredentials
 
 
-@task(retries=3)
+@task(
+    log_prints=True,
+    retries=3,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
-    gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
-    return Path(f"../data/{gcs_path}")
+    gcs_block = GcsBucket.load("zoomcamp-gcs-bucket")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"from_gcs/")
+    return Path(f"from_gcs/{gcs_path}")
 
 
-@task()
+@task(log_prints=True)
 def transform(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
     df = pd.read_parquet(path)
     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
+    df["passenger_count"] = df["passenger_count"].fillna(0)
     print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 
-@task()
+@task(log_prints=True)
 def write_bq(df: pd.DataFrame) -> None:
     """Write DataFrame to BiqQuery"""
 
-    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+    gcp_credentials_block = GcpCredentials.load("zoomcamp-gcs-creds")
 
     df.to_gbq(
         destination_table="dezoomcamp.rides",
-        project_id="prefect-sbx-community-eng",
+        project_id="focused-brace-374920",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
