ab69054f8cffa59b7aef896984329fb4374f20f0
stanchiu224 <stanchiu224@gmail.com>
2023-02-17 15:20:02 +0000

    revert to using github.load

--- flows/04_homework/gh_deploy.py
+++ flows/04_homework/gh_deploy.py
@@ -2,11 +2,8 @@ from prefect.filesystems import GitHub
 from parameterized_flow_github import etl_parent_flow
 from prefect.deployments import Deployment
 
-github_block = GitHub(
-    repository="https://github.com/stanchiu224/prefect-zoomcamp.git", reference="main"
-)
+github_block = GitHub.load("de-zoomcamp")
 github_block.get_directory("flows/04_homework")
-github_block.save("de-zoomcamp")
 
 github_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
