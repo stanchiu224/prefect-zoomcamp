b1b1ae2528253726a55ce3e0b1a836f5d64b5057
stanchiu224 <stanchiu224@gmail.com>
2023-02-17 14:45:14 +0000

    Added parameters for 11-2020 green taxi data

--- flows/04_homework/flows/04_homework/gh_deploy.py
+++ flows/04_homework/flows/04_homework/gh_deploy.py
@@ -3,12 +3,9 @@ from parameterized_flow_github import etl_parent_flow
 from prefect.deployments import Deployment
 
 github_block = GitHub.load("zoomcamp")
-github_block.get_directory("flows/04_homework")
 
 github_dep = Deployment.build_from_flow(
-    flow=etl_parent_flow,
-    name="github-to-gcs-flow",
-    parameters={"color": "green", "months": [11], "years": [2020]},
+    flow=etl_parent_flow, name="github-to-gcs-flow", infrastructure=github_block
 )
 
 if __name__ == "__main__":
